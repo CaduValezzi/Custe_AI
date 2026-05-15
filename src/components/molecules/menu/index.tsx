693769a79e6b7384bc3f0fcb54b8e20906520e3a
Cassio <cassioe.valezzi@gmail.com>
2026-05-15 01:44:15 -0300

    feat(menu): enhance scroll behavior and update styles for improved UX

--- src/components/molecules/menu/index.tsx
+++ src/components/molecules/menu/index.tsx
@@ -7,14 +7,19 @@ import { Logo } from "@/components/atoms/logo";
 
 export const Menu = (): ReactNode => {
     useEffect(() => {
+        let lastScrollY: number = 0;
         const handleScroll = () => {
-            const menu = document.querySelector(`.${S.menu__section}`);
-            if (window.scrollY > 0) {
-                menu?.classList.add(S.menu__section__scrolled);
-                menu?.classList.remove(S.menu__section__notscrolled);
+           
+            const windowScrollY = window.scrollY;
+        
+            const section = document.querySelector(`.${S.menu__section}`);
+            
+            if (windowScrollY > lastScrollY) {
+                lastScrollY = windowScrollY;
+                section?.classList.add(S.menu__section__scrolled);
             } else {
-                menu?.classList.remove(S.menu__section__scrolled);
-                menu?.classList.add(S.menu__section__notscrolled);
+                lastScrollY = windowScrollY;
+                section?.classList.remove(S.menu__section__scrolled);
             }
         };
 
@@ -27,18 +32,18 @@ export const Menu = (): ReactNode => {
 
   return (
     <>
-        <div className={`${S.menu__section} ${S.menu__section__notscrolled}`}>
-            <div className={S.menu__container}>
+        <div className={`${S.menu__section} `}>
+            <div className={`${S.menu__container}`}>
                 <Logo alt="Logo" size="small" />
                 <nav className={S.menu__nav}>
                   <ul className={S.menu__list}>
                     <li className={S.menu__item}><a href="#home" rel="noopener noreferrer" >Home</a></li>
-                    <li className={S.menu__item}><a href="#about" rel="noopener noreferrer" >Funcionalidades</a></li>
+                    <li className={S.menu__item}><a href="#feature" rel="noopener noreferrer" >Funcionalidades</a></li>
                     <li className={S.menu__item}><a href="#contact" rel="noopener noreferrer" >Contact</a></li>
                   </ul>
                 </nav>
-                <button>Menu</button>
             </div>
+            <button className={S.menu__button}>Login</button>
         </div >
     </>
   );
