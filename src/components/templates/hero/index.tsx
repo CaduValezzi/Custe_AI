cf0e1d8e75f4b7533c2d21878ea5eb92f28c9487
Cassio <cassioe.valezzi@gmail.com>
2026-05-19 14:41:28 -0300

    feat(hero): refactor HeroTemplate structure and enhance styles for improved layout

--- src/components/templates/hero/index.tsx
+++ src/components/templates/hero/index.tsx
@@ -1,14 +1,36 @@
 import { Picture } from "@/components/atoms/picture";
 import HeroImage from "../../../../public/images/pexels-weekendplayer-186461.jpg";
+import S from "./styles.module.scss"
 
 export const HeroTemplate = () => {
   return (
     <>
-      <div className="hero__section">
-        <div className="hero__container" >
-          <Picture src={HeroImage.src} sizeHeight="100vh" sizeWidth="100vw"/>
+      <section className={S.hero__section}>
+        <div className={S.hero__container}>
+          <div className={S.hero__content}>
+
+            <div className={S.hero__info}>
+              <h1 className={S.hero__headline}>
+                Controle todos os custos das suas APIs em um só lugar.
+              </h1>
+
+              <p className={S.hero__description}>
+                Monitore gastos, receba alertas e visualize o consumo da sua stack em tempo real.
+              </p>
+
+              <div className={S.hero__actions}>
+                <button>Começar agora!</button>
+                <button>Ver dashboard</button>
+              </div>
+            </div>
+
+            <div className={S.hero__visual}>
+               <Picture src={HeroImage.src} sizeHeight="50vh" sizeWidth="100vw"/>
+            </div>
+
+          </div>
         </div>
-      </div>
+      </section>
     </>
   )
 }; 
