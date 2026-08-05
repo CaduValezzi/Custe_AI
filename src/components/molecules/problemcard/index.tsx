8908e5f6ac524c0c04d800c3e11555c87925c1a2
Cadu Valezzi <caduvalezzi@gmail.com>
2026-08-05 11:29:45 -0300

    refactor(problem): remove unused animation logic and clean up code

--- src/components/molecules/problemcard/index.tsx
+++ src/components/molecules/problemcard/index.tsx
@@ -1,11 +1,14 @@
-'use client'
 import { forwardRef } from "react";
 import { ProblemCardProps } from "./props"
 import S from "./styles.module.scss"
+import { cardAnimation } from "./animation";
 
 export const ProblemCard = forwardRef< HTMLDivElement,ProblemCardProps > (({ icon, title, content }, ref) =>{
+  const cardRef = ref ?? ((node: HTMLDivElement | null) => {
+    if (node) cardAnimation(node);
+  });
     return (
-    <div ref={ref} className={S.problemcard}>
+    <div ref={cardRef} className={S.problemcard}>
       <span className={S.problemcard__icon}>{icon}</span>
       <h3 className={S.problemcard__title}>{title}</h3>
       <p className={S.problemcard__content}>{content}</p>
--- src/components/templates/home/problem/index.tsx
+++ src/components/templates/home/problem/index.tsx
@@ -1,29 +1,12 @@
-"use client";
-
-import { useEffect, useRef } from "react";
 import { Title } from "@/components/atoms/title";
 import { Section } from "@/components/organisms/section";
 import { problems } from "./const"
-import {cardsAnimation } from "./helpers";
 import { Eyebrow } from "@/components/atoms/eyebrow";
 import { Sub } from "@/components/atoms/sub"
 import { ProblemCard } from "@/components/molecules/problemcard";
 import S from "./styles.module.scss";
 
-
-
 export const ProblemSection = () => {
-  const cardsRef = useRef<HTMLDivElement[]>([]);
-
-  useEffect(() => {
-  if (!cardsRef.current) return;
-  const cardsCleanup = cardsAnimation(cardsRef.current);
-
-  return () => {
-    cardsCleanup();
-  };
-}, []);
-
   return (
     <Section id="problem">
       <div className={S.problem}>
@@ -39,15 +22,10 @@ export const ProblemSection = () => {
           </div>
 
           <div className={S.problem__grid}>
-            {problems.map((problem, index) => (
-              
+            {problems.map((problem) => (
               <ProblemCard
-              ref={(el) => {
-                  if (el) cardsRef.current[index] = el;
-                }}
               key={problem.title} icon={problem.icon} title={problem.title} content={problem.content} />
             ))}
-  
           </div>
         </div>
       </div>
