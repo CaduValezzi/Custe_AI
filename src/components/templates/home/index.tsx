cf8c491b30b99e68499dbcf671fbf334c7a5b3b8
Cadu Valezzi <caduvalezzi@gmail.com>
2026-08-05 11:29:55 -0300

    refactor(home): simplify HomeTemplate by removing layered pinning logic

--- src/components/templates/home/index.tsx
+++ src/components/templates/home/index.tsx
@@ -1,23 +1,12 @@
 "use client";
 
 import { useEffect, useRef } from "react";
-import { layeredPinning } from "./helpers";
 import { HeroSection } from "@/components/templates/home/hero";
 import { ProblemSection } from "@/components/templates/home/problem";
 import { Menu } from "@/components/organisms/menu";
 import { SolutionSection } from "./solution";
 
 export const HomeTemplate = () => {
-  const sectionsRef = useRef<HTMLDivElement[]>([]);
-
-  useEffect(() => {
-    const cleanup = layeredPinning(sectionsRef.current);
-
-    return () => {
-      cleanup();
-    };
-  }, []);
-
   return (
     <>
       <Menu />
